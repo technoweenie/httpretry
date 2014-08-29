b88381cd2b221d559fe8f94ca9ff600488ffa675
Rick Olson <technoweenie@gmail.com>
2014-08-29 10:01:48 -0700

    backoff docs

--- backoff.go
+++ backoff.go
@@ -6,10 +6,14 @@ import (
 	"github.com/cenkalti/backoff"
 )
 
+// DefaultBackOff returns a new backoff.BackOff that's used by default for ever
+// new *HttpGetter.
 var DefaultBackOff = func() backoff.BackOff {
 	return backoff.NewExponentialBackOff()
 }
 
+// QuittableBackOff is a backoff.BackOff that halts future retries after Done()
+// gets called.
 type QuittableBackOff struct {
 	b      backoff.BackOff
 	IsDone bool
