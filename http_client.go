18115dab2bc3fc0a50dd3ad4dbeae6e810dfe997
risk danger olson <technoweenie@gmail.com>
2016-05-13 07:40:36 -0600

    add DialWithTimeouts that takes separate values

--- http_client.go
+++ http_client.go
@@ -14,10 +14,21 @@ import (
 // https://github.com/rlmcpherson/s3gof3r/blob/1e759738ff170bd0381a848337db677dbdd6aa62/http_client.go
 //
 func ClientWithTimeout(timeout time.Duration) *http.Client {
+	return ClientWithTimeouts(timeout, timeout, timeout)
+}
+
+// ClientWithTimeout is an http client optimized for high throughput.  It times
+// out more agressively than the default http client in net/http as well as
+// setting deadlines on the TCP connection.
+//
+// Taken from s3gof3r:
+// https://github.com/rlmcpherson/s3gof3r/blob/1e759738ff170bd0381a848337db677dbdd6aa62/http_client.go
+//
+func ClientWithTimeouts(dialTimeout, kaTimeout, actTimeout time.Duration) *http.Client {
 	transport := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
-		Dial:  DialWithTimeout(timeout),
-		ResponseHeaderTimeout: timeout,
+		Dial:  DialWithTimeouts(dialTimeout, kaTimeout, actTimeout),
+		ResponseHeaderTimeout: actTimeout,
 		MaxIdleConnsPerHost:   10,
 	}
 	return &http.Client{Transport: transport}
@@ -27,16 +38,23 @@ func ClientWithTimeout(timeout time.Duration) *http.Client {
 // inactivity timeout for the given duration.  This is designed for long running
 // HTTP requests.
 func DialWithTimeout(timeout time.Duration) func(netw, addr string) (net.Conn, error) {
+	return DialWithTimeouts(timeout, timeout, timeout)
+}
+
+// DialWithTimeouts creates a Dial function that returns a connection with
+// separate timeouts for dialing the connection, maintaining the keep-alive
+// connection, and inactivity.  This is designed for long running HTTP requests.
+func DialWithTimeouts(dialTimeout, kaTimeout, actTimeout time.Duration) func(netw, addr string) (net.Conn, error) {
 	return func(netw, addr string) (net.Conn, error) {
-		c, err := net.DialTimeout(netw, addr, timeout)
+		c, err := net.DialTimeout(netw, addr, dialTimeout)
 		if err != nil {
 			return nil, err
 		}
 		if tc, ok := c.(*net.TCPConn); ok {
 			tc.SetKeepAlive(true)
-			tc.SetKeepAlivePeriod(timeout)
+			tc.SetKeepAlivePeriod(kaTimeout)
 		}
-		return &deadlineConn{timeout, c}, nil
+		return &deadlineConn{actTimeout, c}, nil
 	}
 }
 
